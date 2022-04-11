d29717d71965dd8d836e9bbf9595f08baed46783
David Roberts <david.roberts1@sinclair.edu>
2022-04-11 18:10:55 -0400

    The edited version of Bad Math

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,16 +4,20 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	// in the original it only divided the last number by three
+	// but now that i put parenthesis around the three numbers
+	// it will divide all of them by 3
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
 	int n1 =0 , n2 = 0, n3 = 0;
+	// changed up the prompts a bit to clarify that this is input
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Please enter the first number: "; cin >> n1;
+	cout << "Plase enter the second number: "; cin >> n2;
+	cout << "Please enter the third number: "; cin >> n2;
 
 	float a = average(n1, n2, n3);
 
